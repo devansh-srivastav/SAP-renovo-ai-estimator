0531ad708e9aeb3caa67aceb0e57f61da5341984
Devansh Srivastav <devansh.srivastav@gmail.com>
2022-03-23 14:01:20 +0100

    response array

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ app.add_middleware(
 
 @app.get('/predict')
 async def get_hotels(loss_of_capacity: int, managnese_cathode_voltage_conduction: int, temperature_during_high_performance: int, cobalt_anode_voltage_conduction: int, internal_resistance: int, internal_humidity: int):
-    return json.loads(get_prediction(loss_of_capacity, managnese_cathode_voltage_conduction, temperature_during_high_performance, cobalt_anode_voltage_conduction, internal_resistance, internal_humidity).to_json(orient='records'))
+    return json.loads(get_prediction(loss_of_capacity, managnese_cathode_voltage_conduction, temperature_during_high_performance, cobalt_anode_voltage_conduction, internal_resistance, internal_humidity).to_json(orient='records'))[0]
